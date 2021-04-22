8f28237b05bed1e3f6a36bd6844fee6241e419bd
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2021-04-22 18:23:29 -0300

    Add classe de conexao, dao, entity e atualização das pastas do projeto

---
+++ src/test/conexao/ConnectionFactoryTest.java
@@ -0,0 +1,13 @@
+package test.conexao;
+
+import java.sql.Connection;
+
+public class ConnectionFactoryTest {
+    
+    private Connection connection;
+    
+    private void conexaoTest(){
+        
+    }
+    
+}
