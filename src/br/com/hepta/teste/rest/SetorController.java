b76320ea59de3d84609a20db4691a98e0d846783
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2021-04-26 18:09:51 -0300

    Adição da camada Rest

---
+++ src/br/com/hepta/teste/rest/SetorController.java
@@ -0,0 +1,64 @@
+package br.com.hepta.teste.rest;
+
+
+import br.com.hepta.teste.entity.Setor;
+import br.com.hepta.teste.service.SetorService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/setor")
+public class SetorController {
+
+    @Context
+    private HttpServletRequest request;
+
+    @Context
+    private HttpServletResponse response;
+
+    private SetorService setorService;
+
+    public SetorController() {
+        setorService = new SetorService();
+    }
+
+    protected void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    @Path("/")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTodosSetores() {
+        return setorService.listaSetores();
+    }
+
+    @Path("/")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response adicionarSetor(Setor setor) {
+        return setorService.adicionarSetor(setor);
+    }
+
+    @Path("/{id}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response atualizarSetor(@PathParam("id") Integer idSetor, Setor setor) {
+        return setorService.atualizarSetor(idSetor, setor);
+    }
+
+    @Path("/{id}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletarSetor(@PathParam("id") Integer idSetor) {
+       return setorService.deletarSetor(idSetor);
+    }
+
+}
