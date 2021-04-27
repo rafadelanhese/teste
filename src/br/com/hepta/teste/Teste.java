0f260841f9c3c755a0caa7b5684e04315d13430b
Rafael Delanhese <rdmmail-1@yahoo.com.br>
2021-04-27 17:48:17 -0300

    Main zerado

--- src/br/com/hepta/teste/Teste.java
+++ src/br/com/hepta/teste/Teste.java
@@ -1,23 +1,8 @@
 package br.com.hepta.teste;
 
-import java.util.List;
-import java.util.stream.Stream;
-
-import br.com.hepta.teste.dados.SetorDados;
-import br.com.hepta.teste.dao.SetorDAO;
-import br.com.hepta.teste.entity.Setor;
-
 public class Teste {
 
     public static void main(String[] args) {
-
-        SetorDados setorDados = new SetorDados();
-
-        setorDados.criarSetor();
-        setorDados.listagem();
-        setorDados.procurarPorId();
-        setorDados.procurarPorNome();
-        setorDados.fecharConexao();
     }
 
 
